e86bfb8d3f86cf7b1dbadeb437c6dc11584344ad
alexanderelmuratov <elmuratov@gmail.com>
2023-06-03 13:50:26 +0300

    fixes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 import backgroundImage from '../images/background.jpg';
 import defaultLogo from '../images/logo.svg';
@@ -10,11 +10,29 @@ export default function HomePage() {
 
   return (
     <HomePageWrapper>
-      <img src={logo} alt="logo" width={100} style={{ marginBottom: 30 }} />
-      <Typography variant="h1" sx={{ color: 'crimson', fontWeight: 700 }}>
+      <Box sx={{ display: { xs: 'none', md: 'block' } }}>
+        <img src={logo} alt="logo" width={100} style={{ marginBottom: 30 }} />
+      </Box>
+      <Typography
+        variant="h1"
+        sx={{
+          marginBottom: '20px',
+          color: 'crimson',
+          fontWeight: 700,
+          textAlign: 'center',
+          fontSize: { xs: '3rem', lg: '6rem' },
+        }}
+      >
         Поделись моментами
       </Typography>
-      <Typography variant="h2" sx={{ color: 'teal' }}>
+      <Typography
+        variant="h2"
+        sx={{
+          color: 'teal',
+          textAlign: 'center',
+          fontSize: { xs: '2rem', lg: '4rem' },
+        }}
+      >
         Поделись жизнью
       </Typography>
     </HomePageWrapper>
@@ -23,12 +41,15 @@ export default function HomePage() {
 
 const HomePageWrapper = styled.div`
   height: calc(100vh - 60px);
-  padding-top: 400px;
+  padding-top: 80px;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
   background-image: url(${backgroundImage});
 `;
+// padding-top: 400px;
+// margin-top: auto;
