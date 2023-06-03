a12937cb73072bba9c05d80e7a8f36fb5f0534a0
alexanderelmuratov <elmuratov@gmail.com>
2023-06-03 14:18:15 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import { Box, Typography } from '@mui/material';
 
 import backgroundImage from '../images/background.jpg';
@@ -9,47 +8,50 @@ export default function HomePage() {
   const logo = defaultLogo;
 
   return (
-    <HomePageWrapper>
-      <Box sx={{ display: { xs: 'none', md: 'block' } }}>
-        <img src={logo} alt="logo" width={100} style={{ marginBottom: 30 }} />
+    <Box sx={styles.homePageWrapper}>
+      <Box sx={styles.logoWrapper}>
+        <img src={logo} alt="logo" width={100} />
       </Box>
-      <Typography
-        variant="h1"
-        sx={{
-          marginBottom: '20px',
-          color: 'crimson',
-          fontWeight: 700,
-          textAlign: 'center',
-          fontSize: { xs: '3rem', lg: '6rem' },
-        }}
-      >
+      <Typography variant="h1" sx={styles.title}>
         Поделись моментами
       </Typography>
-      <Typography
-        variant="h2"
-        sx={{
-          color: 'teal',
-          textAlign: 'center',
-          fontSize: { xs: '2rem', lg: '4rem' },
-        }}
-      >
+      <Typography variant="h2" sx={styles.subTitle}>
         Поделись жизнью
       </Typography>
-    </HomePageWrapper>
+    </Box>
   );
 }
 
-const HomePageWrapper = styled.div`
-  height: calc(100vh - 60px);
-  padding-top: 80px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-image: url(${backgroundImage});
-`;
-// padding-top: 400px;
-// margin-top: auto;
+const styles = {
+  homePageWrapper: {
+    height: 'calc(100vh - 60px)',
+    paddingTop: '160px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundImage: `url(${backgroundImage})`,
+  },
+  logoWrapper: {
+    display: {
+      xs: 'none',
+      md: 'block',
+    },
+    marginBottom: '50px',
+  },
+  title: {
+    marginBottom: '20px',
+    color: 'crimson',
+    fontWeight: 700,
+    textAlign: 'center',
+    fontSize: { xs: '3rem', lg: '6rem' },
+  },
+  subTitle: {
+    color: 'teal',
+    textAlign: 'center',
+    fontSize: { xs: '2rem', lg: '4rem' },
+  },
+};
